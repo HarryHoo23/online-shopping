091885f8dbb3b33ff76f0f6dbae860372423bf35
Harry Hu <hoozh1123@gmail.com>
2021-07-11 18:27:52 +1000

    first init

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+  Home,
+  About,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Private,
+  ErrorPage,
+  Products
+} from './pages';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/cart' component={Cart} />
+        <Route exact path='/products' component={Products} />
+        <Route exact path='/products/:id' children={<SingleProduct />} />
+        <Route exact path='/checkout' component={Checkout} />
+        <Route path='*' component={ErrorPage} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './HomePage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import Cart from './CartPage';
+import ErrorPage from './ErrorPage';
+import Checkout from './CheckoutPage';
+import About from './AboutPage';
+import Private from './PrivateRoute';
+
+export {
+  Home,
+  About,
+  Products,
+  SingleProduct,
+  Cart,
+  ErrorPage,
+  Checkout,
+  Private
+};
