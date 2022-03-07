42e264f9d16e26751d982244ea652d687b2dae22
HarryHoo23 <hoozh1123@gmail.com>
2022-03-07 13:31:07 +1100

    Add sort functionality.

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,24 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  return <h4>product list</h4>
+  const { filtered_products: products, grid_view: gridView } = useFilterContext();
+
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: 'none' }}>
+        Sorry, no products matched your search...
+      </h5>
+    )
+  }
+
+  if (gridView === false) {
+    return <ListView products={products} />  
+  }
+  return (
+    <GridView products={products}>
+      product list
+    </GridView>
+  )
 }
 
 export default ProductList
