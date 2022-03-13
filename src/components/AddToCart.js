ab6329b94c7e4c0658adcc171dab61f4495e50cb
Harry Hu <hoozh1123@gmail.com>
2022-03-14 10:43:46 +1100

    addToCart function passed down.

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -7,6 +7,7 @@ import AmountButtons from './AmountButtons'
 
 const AddToCart = ({product}) => {
   const { id, stock, colors } = product;
+  const { addToCart } = useCartContext();
   
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
@@ -38,7 +39,7 @@ const AddToCart = ({product}) => {
     </div>
     <div className="btn-container">
       <AmountButtons amount={amount} increase={increase} decrease={decrease} />
-      <Link to="/cart" className="btn">add to cart</Link>
+      <Link to="/cart" className="btn" onClick={() => addToCart(id, mainColor, amount, product )}>add to cart</Link>
     </div>
   </Wrapper>)
 }
