7820bc58b8f11f92d2dbf73363d410248c22a3c5
Harry Hu <hoozh1123@gmail.com>
2022-04-04 23:15:12 +1000

    Ready to deploy

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <Auth0Provider
-    domain='dev-5a6tmbuw.au.auth0.com'
-    clientId='e4K2lcGmLKh89edI5DfutuGK0LdUxp7N'
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
